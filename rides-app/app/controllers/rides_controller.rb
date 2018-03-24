d5ea2568ebf10cd7ce505399051dabb36154ecca
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-24 12:50:23 -0400

    Add New Ride form, error partial

--- rides-app/app/controllers/rides_controller.rb
+++ rides-app/app/controllers/rides_controller.rb
@@ -8,4 +8,31 @@ class RidesController < ApplicationController
     @ride = Ride.find(params[:id])
   end
 
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.new
+
+    @ride.title = params[:ride][:title]
+    @ride.date = params[:ride][:date]
+    @ride.duration = params[:ride][:duration]
+    @ride.distance = params[:ride][:distance]
+    @ride.location = params[:ride][:location]
+    @ride.intensity = params[:ride][:intensity]
+    @ride.drop = params[:ride][:drop]
+    @ride.number_of_riders = params[:ride][:number_of_riders]
+    @ride.elevation_gain = params[:ride][:elevation_gain]
+    @ride.description = params[:ride][:description]
+    @ride.image = params[:ride][:image]
+    @ride.owner = current_user
+
+    if @ride.save
+      redirect_to rides_url
+    else
+      render :new
+    end
+  end
+
 end
