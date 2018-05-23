0d0a235ffdd38acc3c0a691b072fc29c669489b1
dmitrysbn <dmitry.sbn@gmail.com>
2018-05-23 11:03:03 -0400

    Add more tests to Ride

--- group-ride/spec/models/ride_spec.rb
+++ group-ride/spec/models/ride_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Ride, type: :model do
     expect(@ride).to be_valid
   end
 
+  it "is not valid without a title" do
+    @ride = build(:ride, title: nil, organizer: @user)
+
+    expect(@ride).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    @ride = build(:ride, date: nil, organizer: @user)
+
+    expect(@ride).to_not be_valid
+  end
+
+  it "is not valid without a duration" do
+    @ride = build(:ride, duration: nil, organizer: @user)
+
+    expect(@ride).to_not be_valid
+  end
+
 end
